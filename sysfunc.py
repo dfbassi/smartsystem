5a316b1e46758c95ea2648c032c35a8fc32b06d1
dfbassi <danilo.bassi@gmail.com>
2016-09-27 16:32:11 -0700

    sysfunc: toExpress moved

--- sysfunc.py
+++ sysfunc.py
@@ -12,36 +12,13 @@ Created on Wed Sep 14 19:56:51 2016
     
 """
 
-import tokenizer as tk
-import parser    as ps
-import expr
 
 
-
-def ToExpr(st,h=None) :         # ToExpr converts string into an expression
-    tok = tk.Token(st)
-    if h == 1 :                             # Looking for one expression
-        exp = ps.parseExpr(tok)
-        if exp :
-            return exp
-        else :
-            return expr.Symbol("Null")
-    elif type(h) == str :                   # Looking for all expressions
-        exp = expr.Compound(expr.Symbol(h)) # Compound expression head h
-    else :
-        exp = []                            # List of expressions
-    while tok.size() :
-        exp.append(ps.parseExpr(tok))
-    return exp
-    
 def ReadStr(flname) :           # ReadStr reads file into a string    
     fl = open(flname)
     s = fl.read()
     fl.close()
     return s                    # returns string read
-    
-def Read(flname,h=None):        # Read 
-    return ToExpr(ReadStr(flname),h)
     
 def Eval(exp) :                 # Evaluation of expression
     return exp
