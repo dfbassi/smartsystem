a072ddaa27da70121234a10e9ac1db78d108a878
dfbassi <danilo.bassi@gmail.com>
2016-09-28 17:51:24 -0700

    parser: modif. for sequence

--- parser.py
+++ parser.py
@@ -15,6 +15,8 @@
 class Parse(object):
     def __init__(self,s):
         self.sys = s                        # system instantiation: token, expr
+        print "List :", self.sys.expre.list
+        print "Null :", self.sys.expre.null
         
     def parse(self,par):
         if type(par) == str :               # string as parameter
@@ -23,7 +25,7 @@ class Parse(object):
         if exp :                            # verification
             return exp
         else:
-            return self.sys.expre.Symbol('Null')
+            return self.sys.expre.null
 
     def parseExpr(self,tk,pr=0):
         if tk.typ()== "newline" :           # leading newline eliminated
@@ -73,19 +75,19 @@ class Parse(object):
 
     def parseList(self,tk) :
         e = self.parseLimSeq(tk,'{','}')    # try parsing a list:{e1,e2,..}
-        if e :
-            e.prepend(self.sys.expre.Symbol("List"))  # converting into expression List
+        if e!=None :
+            e.prepend(self.sys.expre.list)  # converting into expression List
             return e
 
     def parseSymbTail(self,tk,h) :          # h is the head, so far
         e = self.parseLimSeq(tk,'[',']')    # try parsing argument list: [e1,e2,..]
-        if e :                                  # is expresion sequence
+        if e != None :                      # is expresion sequence
             e.prepend(h)
             return self.parseSymbTail(tk,e) # try parsing symbol tail (current exp)
         e = self.parseLimSeq(tk,'[[',']]')  # try parsing part list: [[n1,n2,...]]
-        if e :
+        if e != None :
             e.prepend(h)
-            e.prepend(self.sys.expre.Symbol("Part"))
+            e.prepend(self.sys.expre.part)
             return self.parseSymbTail(tk,e) # try parsing symbol tail (current exp)
         return h                                # no tail: returning head (h)
 
@@ -93,14 +95,18 @@ class Parse(object):
         if tk.val()== left :                # comparing with left delimiter
             tk.popVal()                     # left delimiter is popped
             e = self.parseSeq(tk)           # parsing a sequence
-            if e and tk.val()== right :     # comparing with right delimiter 
+            if e!= None and tk.val()== right :     # comparing with right delimiter 
                 tk.popVal()                 # right delimiter is popped
             else :                          # syntax error!
                 print "Syntax error, parsing "+left+"..."
             return e                        # sequence returned
         
     def parseSeq(self,tk):
-        return self.parseSeqTail(tk,self.sys.expre.Sequence(self.parseExpr(tk)))
+        e = self.parseExpr(tk)
+        if e!=None :
+            return self.parseSeqTail(tk,self.sys.expre.Sequence([e]))
+        else :
+            return self.sys.expre.Sequence([ ])
 
     def parseSeqTail(self,tk,e):
         while tk.val()== ',' :              # delimiter comparison
@@ -129,15 +135,16 @@ class Parse(object):
             if s :
                 if e.head().val!=op.val or not self.sys.isAssoc(op.val):
                     f = e                       # e becomes first expression     
-                    e = self.sys.expre.Sequence(op)# op head of expression sequence
+                    e = self.sys.expre.Sequence([op])# op head of expression sequence
                     e.append(f)                 # adding first expression  
                 e.append(s)                     # adding second  expression
             else :
-                print "Syntax error, missing expression afer: "+op.val              
+                print "Syntax error, missing expression afer: "+op.show()              
             op = self.nextOp("Infix",tk,pr)     # next operator (if priority ≥ pr )
         return e
 
     def nextOp(self,typ,tk,pr):
+#        print "trying nextOp : first"
         if tk.impliedProd() and self.sys.prior("*") >= pr:
             return self.sys.expre.Symbol("*")   # getting product with empty token
         if self.sys.isOperator(tk.val(),typ) and self.sys.prior(tk.val()) >= pr:
